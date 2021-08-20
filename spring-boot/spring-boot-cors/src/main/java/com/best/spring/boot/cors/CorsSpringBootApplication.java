94b4af36a313b35e92abf377a7139cd82201afd4
王存露 <1137738840@qq.com>
2021-08-20 22:47:29 +0800

    增加 跨域

---
+++ spring-boot/spring-boot-cors/src/main/java/com/best/spring/boot/cors/CorsSpringBootApplication.java
@@ -0,0 +1,11 @@
+package com.best.spring.boot.cors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CorsSpringBootApplication.class, args);
+    }
+}
