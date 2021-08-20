4b064ff5c1a462272dd07d06081827437bc17694
王存露 <1137738840@qq.com>
2021-08-20 22:46:50 +0800

    增加 rabbitmq

--- spring-boot/spring-boot-rabbitmq/src/main/java/com/best/spring/rabbitmq/SpringBootRabbitMqDemo.java
+++ spring-boot/spring-boot-rabbitmq/src/main/java/com/best/spring/rabbitmq/SpringBootRabbitMqDemo.java
@@ -1,12 +1,12 @@
 package com.best.spring.rabbitmq;
 
 import com.best.spring.rabbitmq.model.Demo1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.messaging.handler.annotation.Payload;
-
-import javax.annotation.PostConstruct;
 
 /**
  * @author 王存露
@@ -18,9 +18,13 @@ public class SpringBootRabbitMqDemo {
         SpringApplication.run(SpringBootRabbitMqDemo.class, args);
     }
 
-    @RabbitListener(queues = "queue2")
-    public void rce(@Payload Demo1 demo1) {
+    @RabbitListener(queues = "queue2", concurrency = "2-5")
+    public void rce(@Payload Demo1 demo1) throws InterruptedException {
+        Logger logger = LoggerFactory.getLogger(SpringBootRabbitMqDemo.class);
+        logger.info("正在消费");
+        Thread.sleep(1000000000);
         System.out.println(demo1.toString());
+        logger.info("消费完成");
     }
 
 }
