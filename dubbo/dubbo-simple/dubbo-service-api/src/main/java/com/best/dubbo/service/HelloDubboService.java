ecfc4932b4a00825f321b1f70b811404e04a68d7
王存露 <1137738840@qq.com>
2021-08-20 22:48:08 +0800

    增加dubbo

---
+++ dubbo/dubbo-simple/dubbo-service-api/src/main/java/com/best/dubbo/service/HelloDubboService.java
@@ -0,0 +1,12 @@
+package com.best.dubbo.service;
+
+
+public interface HelloDubboService {
+    /**
+     * 说你好
+     *
+     * @param name
+     * @return
+     */
+    String sayHello(String name);
+}
