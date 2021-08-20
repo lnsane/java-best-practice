6fd3532d4d6f5516618513c89a90c1d1129aae6f
王存露 <1137738840@qq.com>
2021-08-20 22:47:18 +0800

    增加 log4j2

---
+++ spring-boot/spring-boot-log4j2-yml/src/main/java/com/best/spring/boot/log4j2/yml/Log4j2SpringBootApplication.java
@@ -0,0 +1,23 @@
+package com.best.spring.boot.log4j2.yml;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+@Slf4j
+public class Log4j2SpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Log4j2SpringBootApplication.class, args);
+    }
+
+    @PostConstruct
+    public void mai2() {
+        log.info("info");
+        log.error("error");
+        log.warn("warn");
+        log.debug("debug");
+    }
+}
