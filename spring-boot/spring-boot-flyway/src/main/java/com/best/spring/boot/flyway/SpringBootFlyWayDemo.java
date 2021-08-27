74b53c4ec7744004ca8f0f3c2010302ec7b52b22
王存露 <1137738840@qq.com>
2021-08-27 18:28:41 +0800

    新增 flyway

---
+++ spring-boot/spring-boot-flyway/src/main/java/com/best/spring/boot/flyway/SpringBootFlyWayDemo.java
@@ -0,0 +1,11 @@
+package com.best.spring.boot.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlyWayDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFlyWayDemo.class, args);
+    }
+}
