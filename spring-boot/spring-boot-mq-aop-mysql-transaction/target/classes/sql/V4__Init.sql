6e4393f7e53bf33c686af0271da6476f01e6062f
王存露 <1137738840@qq.com>
2021-11-29 22:07:15 +0800

    configure

---
+++ spring-boot/spring-boot-mq-aop-mysql-transaction/target/classes/sql/V4__Init.sql
@@ -0,0 +1,11 @@
+-- auto-generated definition
+create table `order`
+(
+    id          varchar(60)  not null
+        primary key,
+    order_name  varchar(255) null,
+    state       int          null comment '1 , 2, ,3 ',
+    create_time datetime     null,
+    update_time datetime     null
+);
+
