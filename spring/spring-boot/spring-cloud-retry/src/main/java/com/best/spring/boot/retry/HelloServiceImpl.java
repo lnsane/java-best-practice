46504ed8eecfede613e33682bb34e59ce13be58f
王存露 <1137738840@qq.com>
2022-04-06 21:24:15 +0800

    fix: maven setting

--- spring/spring-boot/spring-cloud-retry/src/main/java/com/best/spring/boot/retry/HelloServiceImpl.java
+++ spring/spring-boot/spring-cloud-retry/src/main/java/com/best/spring/boot/retry/HelloServiceImpl.java
@@ -5,18 +5,23 @@ import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 @Service
 public class HelloServiceImpl implements HelloService {
+
 
     @Override
     @Retryable(recover = "helloFail",maxAttempts=3, backoff=@Backoff(delay=100, maxDelay=500) ,value = ArithmeticException.class)
     public String hello() {
-        if (1 / 0 == 9) {
-
-        }
         return "hello";
     }
 
+    public static void main(String[] args) throws InterruptedException {
+         final BlockingQueue<Long> catalogs = new ArrayBlockingQueue<>(1000);
+        System.out.println(catalogs.take());
+    }
     @Recover
     public String helloFail(ArithmeticException e){
         return "fail";
